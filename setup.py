f2c47aacbcf60199ad06e9217bbe52f2c88e41dd
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2025-03-20 12:16:08 -0700

    added project skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oecraft",
+    packages=find_packages(),
+    version="0.0.1",
+    author="Ben Prystawski",
+    author_email="benpry@stanford.edu",
+    description="A langauge model-driven open-ended crafting game.",
+)
