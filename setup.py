6d37a2f47a183b103ea7f93d64fb15fcf913b0c4
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2025-10-16 15:22:09 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="oecraft",
     packages=find_packages(),
-    version="0.0.5",
+    version="0.0.6",
     author="Ben Prystawski",
     author_email="benpry@stanford.edu",
     description="A langauge model-driven open-ended crafting game.",
